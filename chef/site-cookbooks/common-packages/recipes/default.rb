cf95cc2c75d88bd75bc7b14738b7b30a8f9f35fa
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-04 17:34:00 +0900

    chef入れる

---
+++ chef/site-cookbooks/common-packages/recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: common-packages
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "unzip"
+package "iotop"
+package "dstat"
+package "sysstat"
+package "git"
+package "tmux"
+package "jq"
+
