717ac72fa6fe7ccac2edcfc31536ff804141deb1
ichirin2501 <ichirin2501@hatena.ne.jp>
2016-09-09 17:50:33 +0900

    env, initも入れる

--- script/copy-file.sh
+++ script/copy-file.sh
@@ -8,3 +8,6 @@ cp config/sysctl.conf /etc/sysctl.conf
 cp config/redis.conf /etc/redis.conf
 cp config/my.cnf /etc/my.cnf
 cp config/nginx.conf /etc/nginx/nginx.conf
+
+cp init.sh /home/isucon/init.sh
+cp env.sh /home/isucon/env.sh
